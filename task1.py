4ffb7a168cacccc747b4166f93db82a13d5cee75
Rida Rehman <rida.rehman@dci-student.org>
2023-08-24 13:49:50 +0200

    made some changes in task1

--- task1.py
+++ task1.py
@@ -1,15 +1,15 @@
 for x in range(3):
-    num1 = int(input("Enter first Number"))
-    num2 = int(input("Enter second Number"))
+    num1 = int(input("Enter first Number "))
+    num2 = int(input("Enter second Number "))
     if (num1 != num2):
         print("Number1 is not equals to Number2")
-    elif (num1 == num2):
+    else :
         print("Number1 is equals to Number2")
-    elif (num1 >= num2):
-              print("number1 is greater than or equals to number2") 
-    elif (num1 <= num2):
-            print("Number 1 is less than or equals to Number2") 
-    elif (num1 > num2):
-          print("Number1 is greater than number2")
-    elif (num1 < num2):
-           print("num1 is less than num2")
+    if (num1 >= num2):
+        print("number1 is greater than or equals to number2") 
+    else:
+        print("Number 1 is less than or equals to Number2") 
+    if (num1 > num2):
+        print("Number1 is greater than number2")
+    else:
+        print("num1 is less than num2")
