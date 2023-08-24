2d4a20868a849a15cd068e0c0ef5ffe93e38a270
Rida Rehman <rida.rehman@dci-student.org>
2023-08-24 13:41:05 +0200

    Added 2 tasks

---
+++ task1.py
@@ -0,0 +1,15 @@
+for x in range(3):
+    num1 = int(input("Enter first Number"))
+    num2 = int(input("Enter second Number"))
+    if (num1 != num2):
+        print("Number1 is not equals to Number2")
+    elif (num1 == num2):
+        print("Number1 is equals to Number2")
+    elif (num1 >= num2):
+              print("number1 is greater than or equals to number2") 
+    elif (num1 <= num2):
+            print("Number 1 is less than or equals to Number2") 
+    elif (num1 > num2):
+          print("Number1 is greater than number2")
+    elif (num1 < num2):
+           print("num1 is less than num2")
